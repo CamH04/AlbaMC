b0684fda7436f939985bb8cda07f521f29d8f1d4
Cameron <cameronahaynes04@gmail.com>
2025-03-10 19:38:16 +0000

    Initial commit

---
+++ src/main/java/net/alba/albamod/albamc/Albamc.java
@@ -0,0 +1,10 @@
+package net.alba.albamod.albamc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Albamc implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/alba/albamod/albamc/client/AlbamcClient.java
@@ -0,0 +1,10 @@
+package net.alba.albamod.albamc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlbamcClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
