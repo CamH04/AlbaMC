e82fccf092c7fc4b2a999e07c49585ebe6500601
Cameron <cameronahaynes04@gmail.com>
2025-03-11 19:34:33 +0000

    Added Barn Owl Wings

--- src/main/java/net/alba/albamod/albamc/item/ModItems.java
+++ src/main/java/net/alba/albamod/albamc/item/ModItems.java
@@ -3,7 +3,6 @@ import net.alba.albamod.albamc.Albamc;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.alba.albamod.albamc.item.ItemBuilder;
 
 public class ModItems {
     private ModItems(){}
@@ -11,11 +10,16 @@ public class ModItems {
             .setItemGroup(ItemGroups.INGREDIENTS)
             .withSettings(settings -> settings.maxCount(64))
             .buildAndRegister();
+    public static final Item BARN_OWL_WINGS = new ItemBuilder<Item>("barn_owl_wings")
+            .setItemGroup(ItemGroups.INGREDIENTS)
+            .withSettings(settings -> settings.maxCount(1))
+            .buildAndRegister();
 
     public static void registerModItems() {
         Albamc.LOGGER.info("Registering Mod Items for " + Albamc.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(BARN_OWL_FEATHER);
+            entries.add(BARN_OWL_WINGS);
         });
     }
 }
